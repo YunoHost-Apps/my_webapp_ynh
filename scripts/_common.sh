3c2b4bc2a7e9456d986cc00710784eccf8a70e04
Tagadda <36127788+Tagadda@users.noreply.github.com>
2022-01-31 12:48:53 +0000

    configpanel update with_sftp

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -365,3 +365,38 @@ ynh_app_changelog () {
         echo "No significative changes from the changelog..." > "${final_changelog}_lite"
     fi
 }
+
+ynh_system_user_add_group() {
+    # Declare an array to define the options of this helper.
+    local legacy_args=uhs
+    local -A args_array=([u]=username= [g]=groups=)
+    local username
+    local groups
+
+    # Manage arguments with getopts
+    ynh_handle_getopts_args "$@"
+    groups="${groups:-}"
+
+	local group
+	for group in $groups; do
+		usermod -a -G "$group" "$username"
+	done
+}
+
+
+ynh_system_user_del_group() {
+    # Declare an array to define the options of this helper.
+    local legacy_args=uhs
+    local -A args_array=([u]=username= [g]=groups=)
+    local username
+    local groups
+
+    # Manage arguments with getopts
+    ynh_handle_getopts_args "$@"
+    groups="${groups:-}"
+
+	local group
+	for group in $groups; do
+		gpasswd -d "$username" "$group"
+	done
+}
